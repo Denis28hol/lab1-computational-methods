e920616224697c223df846f85e48fa787c2213fa
Denis28hol <122293475+Denis28hol@users.noreply.github.com>
2026-05-07 15:36:59 +0300

    Update api.py

--- api.py
+++ api.py
@@ -4,7 +4,7 @@
 from flask import Flask, request, jsonify
 from pulp import LpProblem, LpVariable, LpMinimize, LpInteger, value
 from itertools import combinations_with_replacement
-
+ 
 app = Flask(__name__)
 
 SIZES = {
