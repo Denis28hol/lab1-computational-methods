c98cc5f0f5fba69e70f1b4c0bf5a828700a5c0aa
Denis28hol <122293475+Denis28hol@users.noreply.github.com>
2026-05-21 16:47:48 +0300

    Update api.py

--- api.py
+++ api.py
@@ -4,7 +4,8 @@
 from flask import Flask, request, jsonify
 from pulp import LpProblem, LpVariable, LpMinimize, LpInteger, value
 from itertools import combinations_with_replacement
- 
+import os
+
 app = Flask(__name__)
 
 SIZES = {
@@ -134,4 +135,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
